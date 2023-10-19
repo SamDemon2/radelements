cd416696e1643e74a71d9c2a1b8e4eda4888bc70
SamDemon_2 <ugay@gmail.com>
2023-10-19 23:04:38 +0300

    Переписываю обработчики событий

--- src/components/IntermediateTable.tsx
+++ src/components/IntermediateTable.tsx
@@ -1,26 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux"; // Импортируем хук useSelector из 'react-redux'
+
 const IntermediateTable = () => {
-  return <>
-    <table className="table table-dark table-striped table-bordered table-hover ">
-      <thead>
-        <tr>
-          <th className="px-0">ID</th>
-          <th className="px-0">Название</th>
-          <th className="px-0">Количество</th>
-        </tr>
-      </thead>
-      <tbody className="text-black">
-          <td>
-            class
-          </td>
-          <td>
-            name
-          </td>
-          <td>
-            amount
-          </td>
-      </tbody>
-    </table>
-  </>
+    // Используем useSelector, чтобы получить данные из хранилища
+    const intermediateComponents = useSelector((state) => state.tableData.inter_components);
+
+    return (
+        <table className="table table-dark table-striped table-bordered table-hover">
+            <thead>
+            <tr>
+                <th className="px-0">ID</th>
+                <th className="px-0">Название</th>
+                <th className="px-0">Количество</th>
+            </tr>
+            </thead>
+            <tbody className="text-black">
+            {intermediateComponents.map((component) => (
+                <tr key={component.category}>
+                    <td>{component.id}</td>
+                    <td>{component.comp_name}</td>
+                    <td>{component.amount}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
 };
+
 export default IntermediateTable;
