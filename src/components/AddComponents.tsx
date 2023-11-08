5864a85c13650cd5e46ae3a7f44d13c225e3b1a9
SamDemon2 <ugay03@gmail.com>
2023-11-08 20:07:12 +0300

    Обработчики событий начали работать!!!!!

--- src/components/AddComponents.tsx
+++ src/components/AddComponents.tsx
@@ -6,23 +6,41 @@ import IntermediateTable from "./IntermediateTable";
 
 const AddComponents = () => {
     const dispatch =useDispatch();
-    const [formData, setFormFata] = useState({
+    const [formData, setFormData] = useState({
         category: "",
         comp_name: "",
         amount: 0,
     })
-    const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setFormFata({
+    const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData({
             ...formData,
             [name]: value,
         });
     };
+
+    const handleCompNameChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
     const handleSubmit = (e:React.FormEvent) => {
       e.preventDefault();
       dispatch(addIntermediateData(formData));
     };
     return <>
+        <form onSubmit={handleSubmit}>
         <div className="ms-3 me-3">
             <div className="row my-3">
                 <div className="row my-3 bold-text d-flex justify-content-start">
@@ -35,14 +53,17 @@ const AddComponents = () => {
                 </div>
                 <div className="col-3">
                     {/*<Dropdown/>*/}
-                    <select className="form-select w-100" aria-label="Default select example">
+                    <select  name="category"
+                             value={formData.category}
+                             onChange={handleCategoryChange}
+                             className="form-select w-100" aria-label="Default select example">
                         <option selected>Chose category</option>
-                        <option value="1">Resistor</option>
-                        <option value="2">Capacitor</option>
-                        <option value="3">NPN Transistor</option>
-                        <option value="4">LED</option>
-                        <option value="5">Relay</option>
-                        <option value="6">Piezoelectric Buzzer</option>
+                        <option value="Resistor">Resistor</option>
+                        <option value="Capacitor">Capacitor</option>
+                        <option value="NPN Transistor">NPN Transistor</option>
+                        <option value="LED">LED</option>
+                        <option value="Relay">Relay</option>
+                        <option value="Piezoelectric Buzzer">Piezoelectric Buzzer</option>
                     </select>
                 </div>
                 <div className='col-2'>
@@ -57,23 +78,26 @@ const AddComponents = () => {
                     comp_name
                 </div>
                 <div className="col-3">
-                    <select className="form-select w-100">
+                    <select  name="comp_name"
+                             value={formData.comp_name}
+                             onChange={handleCompNameChange}
+                             className="form-select w-100">
                         <option selected>Chose name</option>
-                        <option value="1">Resistor 395 Ohms</option>
-                        <option value="2">Capacitor 10 µF</option>
-                        <option value="3">NPN Transistor BC547</option>
-                        <option value="4">5mm Light Emitting Diode (LED)</option>
-                        <option value="5">12V Relay</option>
-                        <option value="6">Piezoelectric Buzzer (3.5 kHz)</option>
-                        <option value="7">Transformer 220V/12V</option>
-                        <option value="8">DS18B20 Temperature Sensor</option>
-                        <option value="9">Optocoupler PC817</option>
-                        <option value="10">Arduino Uno Microcontroller</option>
-                        <option value="11">Tactile Push Button 1</option>
-                        <option value="12">1 kOhm Resistor</option>
-                        <option value="13">100 µF Electrolytic Capacitor</option>
-                        <option value="14">PNP Transistor 2N3906</option>
-                        <option value="15">1N4148 Diode</option>
+                        <option value="Resistor 395 Ohms">Resistor 395 Ohms</option>
+                        <option value="Capacitor 10 µF">Capacitor 10 µF</option>
+                        <option value="NPN Transistor BC547">NPN Transistor BC547</option>
+                        <option value="5mm Light Emitting Diode (LED)">5mm Light Emitting Diode (LED)</option>
+                        <option value="12V Relay">12V Relay</option>
+                        <option value="Piezoelectric Buzzer (3.5 kHz)">Piezoelectric Buzzer (3.5 kHz)</option>
+                        <option value="Transformer 220V/12V">Transformer 220V/12V</option>
+                        <option value="DS18B20 Temperature Sensor">DS18B20 Temperature Sensor</option>
+                        <option value="Optocoupler PC817">Optocoupler PC817</option>
+                        <option value="Arduino Uno Microcontroller">Arduino Uno Microcontroller</option>
+                        <option value="Tactile Push Button 1">Tactile Push Button 1</option>
+                        <option value="1 kOhm Resistor">1 kOhm Resistor</option>
+                        <option value="100 µF Electrolytic Capacitor">100 µF Electrolytic Capacitor</option>
+                        <option value="PNP Transistor 2N3906">PNP Transistor 2N3906</option>
+                        <option value="1N4148 Diode">1N4148 Diode</option>
                     </select>
                 </div>
             </div>
@@ -152,6 +176,7 @@ const AddComponents = () => {
                 </div>
             </div>
         </div>
+        </form>
     </>;
 };
 export default AddComponents;
--- src/components/IntermediateTable.tsx
+++ src/components/IntermediateTable.tsx
@@ -10,7 +10,7 @@ const IntermediateTable = () => {
         <table className="table table-dark table-striped table-bordered table-hover">
             <thead>
             <tr>
-                <th className="px-0">ID</th>
+                <th className="px-0">Категория</th>
                 <th className="px-0">Название</th>
                 <th className="px-0">Количество</th>
             </tr>
