055aebd12a0769e2d093f399cfe91d69af22a21d
SamDemon2 <ugay03@gmail.com>
2023-11-08 19:23:04 +0300

    Вставил тестовые значения в поля выбора категорий и имени

--- src/components/AddComponents.tsx
+++ src/components/AddComponents.tsx
@@ -36,10 +36,13 @@ const AddComponents = () => {
                 <div className="col-3">
                     {/*<Dropdown/>*/}
                     <select className="form-select w-100" aria-label="Default select example">
-                        <option selected>Chose name</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option selected>Chose category</option>
+                        <option value="1">Resistor</option>
+                        <option value="2">Capacitor</option>
+                        <option value="3">NPN Transistor</option>
+                        <option value="4">LED</option>
+                        <option value="5">Relay</option>
+                        <option value="6">Piezoelectric Buzzer</option>
                     </select>
                 </div>
                 <div className='col-2'>
@@ -55,10 +58,22 @@ const AddComponents = () => {
                 </div>
                 <div className="col-3">
                     <select className="form-select w-100">
-                        <option selected>Chose category</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option selected>Chose name</option>
+                        <option value="1">Resistor 395 Ohms</option>
+                        <option value="2">Capacitor 10 µF</option>
+                        <option value="3">NPN Transistor BC547</option>
+                        <option value="4">5mm Light Emitting Diode (LED)</option>
+                        <option value="5">12V Relay</option>
+                        <option value="6">Piezoelectric Buzzer (3.5 kHz)</option>
+                        <option value="7">Transformer 220V/12V</option>
+                        <option value="8">DS18B20 Temperature Sensor</option>
+                        <option value="9">Optocoupler PC817</option>
+                        <option value="10">Arduino Uno Microcontroller</option>
+                        <option value="11">Tactile Push Button 1</option>
+                        <option value="12">1 kOhm Resistor</option>
+                        <option value="13">100 µF Electrolytic Capacitor</option>
+                        <option value="14">PNP Transistor 2N3906</option>
+                        <option value="15">1N4148 Diode</option>
                     </select>
                 </div>
             </div>
@@ -94,9 +109,12 @@ const AddComponents = () => {
                 <div className="col-3 d-flex justify-content-center">
                     <select className="form-select w-100" aria-label="Default select example">
                         <option selected>Chose category</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="1">Resistor</option>
+                        <option value="2">Capacitor</option>
+                        <option value="3">NPN Transistor</option>
+                        <option value="4">LED</option>
+                        <option value="5">Relay</option>
+                        <option value="6">Piezoelectric Buzzer</option>
                     </select>
                 </div>
             </div>
