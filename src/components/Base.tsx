063740282b5ee634f8e155449febba660e40b1a2
SamDemon2 <ugay03@gmail.com>
2023-11-22 01:09:53 +0300

    А вот это рабочая версия, но не через редукс

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../redux/reducers';
-import { fetchDeviceNames } from '../redux/actions';
-
+import axios from "axios";
 const Base = () => {
-    const dispatch = useDispatch();
-    const reduxTableData = useSelector((state: RootState) => state.tableData);
-
-    const [selectedDevice, setSelectedDevice] = useState<string>('');
-    const [amount, setAmount] = useState<string>('');
+    const [deviceNames, setDeviceNames] = useState<string[]>([]);
+    const [selectedDevice, setSelectedDevice] = useState<string>("");
+    const [amount, setAmount] = useState<string>("");
 
     useEffect(() => {
-        // Вызываем fetchDeviceNames при монтировании компонента
-        dispatch(fetchDeviceNames() as any);
-    }, [dispatch]);
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/api/v1/add/');
+                setDeviceNames(response.data.device_names);
+            } catch (error) {
+                console.error('Error fetching device names:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     const handleDeviceChange = (e: ChangeEvent<HTMLSelectElement>) => {
         setSelectedDevice(e.target.value);
@@ -24,62 +27,50 @@ const Base = () => {
         // Ваша логика для обработки данных при отправке формы
     };
 
-    // Проверяем наличие reduxTableData и его свойств перед использованием
-    const deviceNames = reduxTableData?.device_components?.device_names || [];
-
     return (
-        <>
-            <div className="ms-3 me-3">
-                <form onSubmit={handleSubmit}>
-                    <div className="row my-3">
-                        <div className="col-md-2 d-flex">
-                            <span>Select device name</span>
-                        </div>
-                        <div className="col-md-3">
-                            <select
-                                className="form-select w-100"
-                                aria-label="Default select example"
-                                value={selectedDevice}
-                                onChange={handleDeviceChange}
-                            >
-                                <option value="" disabled>Select device name</option>
-                                {deviceNames.length > 0 ? (
-                                    deviceNames.map((item) => (
-                                        <option key={item} value={item}>
-                                            {item}
-                                        </option>
-                                    ))
-                                ) : (
-                                    <option value="" disabled>
-                                        No device names available
-                                    </option>
-                                )}
-                            </select>
-                        </div>
+        <div className="ms-3 me-3">
+            <form onSubmit={handleSubmit}>
+                <div className="row my-3">
+                    <div className="col-md-2 d-flex">
+                        <span>Select device name</span>
                     </div>
-                    <div className="row my-3">
-                        <div className="col-md-2 d-flex">
-                            <span>Select amount</span>
-                        </div>
-                        <div className="col-md-3">
-                            <input
-                                className="form-control w-100"
-                                value={amount}
-                                onChange={(e) => setAmount(e.target.value)}
-                            />
-                        </div>
+                    <div className="col-md-3">
+                        <select
+                            className="form-select w-100"
+                            aria-label="Default select example"
+                            value={selectedDevice}
+                            onChange={handleDeviceChange}
+                        >
+                            <option value="" disabled>Select device name</option>
+                            {deviceNames.map((device, index) => (
+                                <option key={index} value={device}>
+                                    {device}
+                                </option>
+                            ))}
+                        </select>
                     </div>
-                    <div className="row my-3">
-                        <div className="col-md-2 d-flex justify-content-start">
-                            <button type="submit" className="btn btn-primary">
-                                Submit
-                            </button>
-                        </div>
+                </div>
+                <div className="row my-3">
+                    <div className="col-md-2 d-flex">
+                        <span>Select amount</span>
                     </div>
-                </form>
-            </div>
-        </>
+                    <div className="col-md-3">
+                        <input
+                            className="form-control w-100"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="row my-3">
+                    <div className="col-md-2 d-flex justify-content-start">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
     );
 };
-
 export default Base;
